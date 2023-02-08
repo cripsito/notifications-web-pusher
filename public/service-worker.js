d96b6d03a1116667b105e6772fad798e66b5a94a
Cristopher Rojas <cripsito@gmail.com>
2023-02-08 11:05:13 -0600

    modify notif

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,6 @@
-(function () {
-  const img = 'https://random.imagecdn.app/500/150';
-  const text = `HEY! Your task  is now overdue.`;
-  const notification = new Notification('To do list', {
-    body: text,
-    icon: img,
-  });
-})();
+const img = 'https://random.imagecdn.app/500/150';
+const text = `HEY! Your task  is now overdue.`;
+const notification = new Notification('To do list', {
+  body: text,
+  icon: img,
+});

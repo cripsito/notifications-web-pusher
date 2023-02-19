ea8e6952cfd19a90ed08da2c1da50b47ba775dcf
Cristopher Rojas <cripsito@gmail.com>
2023-02-19 12:06:31 -0600

    service worker example

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,54 +1,25 @@
-// urlB64ToUint8Array is a magic function that will encode the base64 public key
-// to Array buffer which is needed by the subscription option
-/* const urlB64ToUint8Array = (base64String) => {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-  const base64 = (base64String + padding)
-    .replace(/\-/g, '+')
-    .replace(/_/g, '/');
-  const rawData = atob(base64);
-  const outputArray = new Uint8Array(rawData.length);
-  for (let i = 0; i < rawData.length; ++i) {
-    outputArray[i] = rawData.charCodeAt(i);
-  }
-  return outputArray;
-};
-const saveSubscription = async (subscription) => {
-  const SERVER_URL = 'http://localhost:4000/save-subscription';
-  const response = await fetch(SERVER_URL, {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(subscription),
+self.addEventListener('install', function (event) {
+  self.registration.showNotification('Bienvenido', {
+    body: 'Gracias por permitirnos enviar notificaciones.',
+    icon: '/path/to/icon.png',
   });
-  return response.json();
-};
-self.addEventListener('activate', async () => { 
-  // This will be called only once when the service worker is installed for first time.
-  try {
-    const applicationServerKey = urlB64ToUint8Array(
-      'BJ5IxJBWdeqFDJTvrZ4wNRu7UY2XigDXjgiUBYEYVXDudxhEs0ReOJRBcBHsPYgZ5dyV8VjyqzbQKS8V7bUAglk'
-    );
-    const options = { applicationServerKey, userVisibleOnly: true };
-    const subscription = await self.registration.pushManager.subscribe(options);
-    const response = await saveSubscription(subscription);
-    console.log(response);
-  } catch (err) {
-    console.log('Error', err);
-  }
-}); */
-self.addEventListener('push', function (event) {
-  if (event.data) {
-    console.log('Push event!! ', event.data.text());
-    showLocalNotification('Yolo', event.data.text(), self.registration);
-  } else {
-    console.log('Push event but no data');
-  }
+  event.waitUntil(self.skipWaiting());
 });
-const showLocalNotification = (title, body, swRegistration) => {
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('push', function (event) {
+  const title = 'Título de la notificación';
   const options = {
-    body,
-    // here you can add more properties like icon, image, vibrate, etc.
+    body: 'Este es el cuerpo de la notificación',
+    icon: './vercel.svg',
   };
-  swRegistration.showNotification(title, options);
-};
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  event.waitUntil(self.clients.openWindow('https://ejemplo.com'));
+});
